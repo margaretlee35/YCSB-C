ec8c4c29d21d14ac108d0cbb092ba926a5b856e3
Margaret Lee <margaretlee@utexas.edu>
2025-05-13 15:41:38 -0500

    lightweight log for basic db

--- db/basic_db.h
+++ db/basic_db.h
@@ -32,7 +32,7 @@ class BasicDB : public DB {
            const std::vector<std::string> *fields,
            std::vector<KVPair> &result) {
     std::lock_guard<std::mutex> lock(mutex_);
-    cout << "READ " << table << ' ' << key;
+    /*cout << "READ " << table << ' ' << key;
     if (fields) {
       cout << " [ ";
       for (auto f : *fields) {
@@ -41,7 +41,8 @@ class BasicDB : public DB {
       cout << ']' << endl;
     } else {
       cout  << " < all fields >" << endl;
-    }
+    }*/
+    cout << "READ " << key << endl;
     return 0;
   }
 
@@ -49,7 +50,7 @@ class BasicDB : public DB {
            int len, const std::vector<std::string> *fields,
            std::vector<std::vector<KVPair>> &result) {
     std::lock_guard<std::mutex> lock(mutex_);
-    cout << "SCAN " << table << ' ' << key << " " << len;
+    /*cout << "SCAN " << table << ' ' << key << " " << len;
     if (fields) {
       cout << " [ ";
       for (auto f : *fields) {
@@ -58,35 +59,39 @@ class BasicDB : public DB {
       cout << ']' << endl;
     } else {
       cout  << " < all fields >" << endl;
-    }
+    }*/
+    cout << "SCAN " << key << endl;
     return 0;
   }
 
   int Update(const std::string &table, const std::string &key,
              std::vector<KVPair> &values) {
     std::lock_guard<std::mutex> lock(mutex_);
-    cout << "UPDATE " << table << ' ' << key << " [ ";
+    /*cout << "UPDATE " << table << ' ' << key << " [ ";
     for (auto v : values) {
       cout << v.first << '=' << v.second << ' ';
     }
-    cout << ']' << endl;
+    cout << ']' << endl;*/
+    cout << "UPDATE " << key << endl;
     return 0;
   }
 
   int Insert(const std::string &table, const std::string &key,
              std::vector<KVPair> &values) {
     std::lock_guard<std::mutex> lock(mutex_);
-    cout << "INSERT " << table << ' ' << key << " [ ";
+    /*cout << "INSERT " << key << " [ ";
     for (auto v : values) {
       cout << v.first << '=' << v.second << ' ';
     }
-    cout << ']' << endl;
+    cout << ']' << endl;*/
+    cout << "INSERT " << key << endl;
     return 0;
   }
 
   int Delete(const std::string &table, const std::string &key) {
     std::lock_guard<std::mutex> lock(mutex_);
-    cout << "DELETE " << table << ' ' << key << endl;
+    //cout << "DELETE " << table << ' ' << key << endl;
+    cout << "DELETE " << key << endl;
     return 0; 
   }
 
